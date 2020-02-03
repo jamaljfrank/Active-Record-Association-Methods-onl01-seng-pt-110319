705b7d214b70daa240bd6edf445c456e63ccc594
Jamal Frank <jamaljfrank@gmail.com>
2020-02-03 20:58:50 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -12,6 +12,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists
+    self.artists.map |a| a.names
+  end
   end
 end
