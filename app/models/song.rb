188b09abda71a00bf81f16fa3f1dceb5ee52e7c2
Jamal Frank <jamaljfrank@gmail.com>
2020-02-03 21:36:22 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
-    if 
+    if self.artist.name.any? {|a| a.name == "Drake"
     
     
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
