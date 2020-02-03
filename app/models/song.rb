fc971d0b0abbcb948237a962a921ff7571cd6460
Jamal Frank <jamaljfrank@gmail.com>
2020-02-03 22:09:43 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     
     
-    if self.artist
+    if !self.artist
       self.artist = drake
     else 
       drake = Artist.create(name: "Drake")
